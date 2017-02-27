2c1387042a1f793d7d2e9eda12359f16d6a21063
TheBoy <theboy5140@gmail.com>
2017-02-27 15:30:42 +0800

    finish game almost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Extra.Tess;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
+    public float speed = 10;
+    public float rotateSpeed = 2;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody2D rb;
+    private int count = 0;
+    private float direction;
 
 	// Use this for initialization
 	void Start () {
 		
+        winText.text = "";
         rb = GetComponent<Rigidbody2D> ();
-	}
+        direction = Mathf.Sign (Random.Range (-1, 1));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,12 +30,33 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis ("Vertical");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb.AddForce (movement * speed);
+        addRotation ();
+
+    }
+
+    void FixedUpdate(){
+        
     }
 
     void OnTriggerEnter2D(Collider2D other){
 
         if (other.gameObject.CompareTag ("PickUp")) {
             other.gameObject.SetActive (false);
+            count += 1;
+            SetCountText ();
         }
+    }
+
+    void SetCountText(){
+        countText.text = "SCORE :　" + count.ToString();
+        if (count >= 12) {
+            winText.text = "YOU WIN!";
+        }
+    }
+
+    void addRotation(){
+
+        Vector3 rotator = new Vector3 (0.0f, 0.0f, 360) * Time.deltaTime * rotateSpeed * direction;
+        transform.Rotate (rotator);
     }
 }
