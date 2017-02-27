29d0428505ea55fe3b701d360cfc3bdd1dfb6935
TheBoy <theboy5140@gmail.com>
2017-02-27 08:29:49 +0800

    add player controller

---
+++ Assets/Scripts/Lib/Person.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Extra{
+
+    namespace Tess{
+
+        class Person{
+
+            private int age;
+
+            public Person(int age){
+                this.age = age;
+            }
+
+            public void say(){
+
+                Debug.Log (" i am " + this.age + " years old");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extra.Tess;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+        rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb.AddForce (movement * speed);
+        Vector2 origin = transform.position;
+        this.transform.position = origin + new Vector2(moveHorizontal, moveVertical);
+        Debug.Log ("horizontal is :" + moveHorizontal);
+        Debug.Log ("vertical is :" + moveVertical);
+    }
+}
